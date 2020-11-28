5303d0b4c047b5f19f2d4b7e3747057cb589b964
Moti Elmakies <motiphone2003@gmail.com>
2020-11-28 21:07:51 +0200

    :apple: redux update completed, working on the item Model.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,12 +3,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../app.css';
 import AppNavbar from './AppNavbar';
 import ShoppingList from './/ShoppingList/ShoppingList';
+import ItemModel from './ItemModel/ItemModel';
 
 const App = () => {
   return (
     <div className='App'>
       <AppNavbar />
+
       <ShoppingList />
+
+      <ItemModel header='This is a header-1'>
+        and this is the content-1!
+      </ItemModel>
     </div>
   );
 };
--- client/src/components/ItemModel/ItemModel.js
+++ client/src/components/ItemModel/ItemModel.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const ItemModel = () => {
-  return <div className='item-model'>Hello</div>;
+import './ItemModel.css';
+
+const ItemModel = ({ header, children }) => {
+  return (
+    <div className='item-model-container'>
+      <div className='item-model-box'>
+        <div className='header'>{header}</div>
+        <div className='content'>{children}</div>
+      </div>
+    </div>
+  );
 };
 
 export default ItemModel;
